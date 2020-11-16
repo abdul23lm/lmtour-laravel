5b3f90900e3677aa529ea41f0fb4aa6144d6e9c2
abdul23lm <abdul23lm@gmail.com>
2020-11-16 09:29:18 +0700

    Fix admin midleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -16,7 +16,7 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::user() && Auth::user()->roles = 'ADMIN') {
+        if (Auth::user() && Auth::user()->roles = 'ADMIN') {
             return $next($request);
         }
         return redirect('/');
